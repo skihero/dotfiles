97c6b10dabcbfaad24af34d573c16c18c9bc6bfa
kishore k <kishore.k@directi.com>
2011-12-07 15:29:35 +0530

    Added a function to convert epoch time to normal time

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -8,6 +8,15 @@ function rpmext (){
 rpm2cpio $1 |cpio -i --make-directories --verbose
 }
 
+function gm { 
+git status |grep modified |awk '{print  "± " $3}'
+echo  ""
+git status |grep deleted | awk '{print  "✗ " $3}'
 
+ }
 
+# Convert epoch to  human date 
+function eph { 
+date -d @${*}
+}
 

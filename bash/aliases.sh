9a27e582368044c8575baca22d641264c1f0719f
kish <skihero@gmail.com>
2011-12-23 04:38:57 +0530

    set to the lowest

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -38,4 +38,4 @@ alias pup='cd /home/kishore/Downloads/clone/puppet/manifests/production/profilea
 alias gp='git pull && echo code pull done|espeak'
 alias gist='python -c "import webbrowser; webbrowser.open(\"https://gist.github.com/\")"'
 alias caps='loadkeys dvorak' # change the file /lib/kbd/keymaps/i386/dvorak/dvorak.map.GZ 
-alias brightlow='sudo pkexec gnome-power-backlight-helper  --set-brightness -1'
+alias brightlow='sudo pkexec gnome-power-backlight-helper  --set-brightness 0'
